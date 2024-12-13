9e61181dd99257d749781d611c0d4626181a3f80
Bilment <tahabilmen@gmail.com>
2024-12-13 15:14:50 -0500

    #6 CategoriesController contructor has been updated.

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,5 +1,6 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,8 +20,13 @@ public class CategoriesController
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
 
-    // create an Autowired controller to inject the categoryDao and ProductDao
+// create an Autowired controller to inject the categoryDao and ProductDao
 
     // add the appropriate annotation for a get action
     public List<Category> getAll()
